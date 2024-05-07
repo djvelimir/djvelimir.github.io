ce067591c71f7b447f0c8c049851b6f105f1781e
Velimir Đurković <velimir.djurkovic@gmail.com>
2024-05-07 23:41:59 +0200

    Remove default home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    { path: 'home', title: 'Home', component: HomeComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '', title: 'Home', component: HomeComponent },
     { path: '**', title: 'Page Not Found', component: PageNotFoundComponent },
 ];
