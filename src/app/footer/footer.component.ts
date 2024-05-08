3accdaf5f2f8384bfc6568b12bb7a8a9ff0c7ea3
Velimir Đurković <velimir.djurkovic@gmail.com>
2024-05-08 18:17:05 +0200

    Split categories

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -8,15 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-  items = [
-    { name: 'Blog', description: '', url: 'https://djvelimir.github.io/blog/'}
-  ];
-
   categories = [
     {
       name: 'Demo Projects',
       items: [
-        { name: 'Blog', url: 'https://djvelimir.github.io/blog/' },
+        { name: 'Hugo', url: 'https://djvelimir.github.io/blog/' },
         { name: 'Angular', url: 'https://djvelimir.github.io/demo-spa-angular/' },
         { name: 'Vue', url: 'https://djvelimir.github.io/demo-spa-vue/' },
         { name: 'React', url: 'https://djvelimir.github.io/demo-spa-react/' },
@@ -24,27 +20,48 @@ export class FooterComponent {
       ]
     },
     {
-      name: 'Source Code',
+      name: 'Frontend',
+      items: [
+        { name: 'Hugo', url: 'https://github.com/djvelimir/blog' },
+        { name: 'Angular', url: 'https://github.com/djvelimir/demo-spa-angular' },
+        { name: 'Vue', url: 'https://github.com/djvelimir/demo-spa-vue' },
+        { name: 'React', url: 'https://github.com/djvelimir/demo-spa-react' },
+        { name: 'Vite + React', url: 'https://github.com/djvelimir/demo-spa-react-vite' },
+      ]
+    },
+    {
+      name: 'Java & Kotlin',
       items: [
         { name: 'Java - CLI', url: 'https://github.com/djvelimir/demo-java-cli' },
         { name: 'Java - Swing', url: 'https://github.com/djvelimir/demo-java-swing' },
         { name: 'Java - FX', url: 'https://github.com/djvelimir/demo-java-fx' },
         { name: 'Java - Spring Boot', url: 'https://github.com/djvelimir/demo-java-spring-boot' },
         { name: 'Kotlin - CLI', url: 'https://github.com/djvelimir/demo-kotlin-cli' },
+      ]
+    },
+    {
+      name: '.NET',
+      items: [
         { name: 'C# .NET - CLI', url: 'https://github.com/djvelimir/demo-dotnet-cli' },
         { name: 'C# .NET - Benchmark', url: 'https://github.com/djvelimir/demo-dotnet-benchmark' },
         { name: 'C# .NET - WebApi', url: 'https://github.com/djvelimir/demo-dotnet-web-api' },
+      ]
+    },
+    {
+      name: 'Mobile',
+      items: [
+        { name: 'Android', url: 'https://github.com/djvelimir/demo-mobile-android' },
+        { name: 'iOS', url: 'https://github.com/djvelimir/demo-mobile-ios' },
+      ]
+    },
+    {
+      name: 'Misc',
+      items: [
         { name: 'C++ - CLI', url: 'https://github.com/djvelimir/demo-cpp-cli' },
         { name: 'Rust', url: 'https://github.com/djvelimir/demo-rust' },
         { name: 'Go', url: 'https://github.com/djvelimir/demo-go' },
         { name: 'Python - CLI', url: 'https://github.com/djvelimir/demo-python-cli' },
         { name: 'Node.js', url: 'https://github.com/djvelimir/demo-node' },
-        { name: 'Angular', url: 'https://github.com/djvelimir/demo-spa-angular' },
-        { name: 'Vue', url: 'https://github.com/djvelimir/demo-spa-vue' },
-        { name: 'React', url: 'https://github.com/djvelimir/demo-spa-react' },
-        { name: 'Vite + React', url: 'https://github.com/djvelimir/demo-spa-react-vite' },
-        { name: 'Android', url: 'https://github.com/djvelimir/demo-mobile-android' },
-        { name: 'iOS', url: 'https://github.com/djvelimir/demo-mobile-ios' },
       ]
     },
   ];
