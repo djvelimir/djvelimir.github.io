256d899dc293261494d9776d7aaf8fd59cffcbe8
Velimir Đurković <velimir.djurkovic@gmail.com>
2024-12-27 22:45:58 +0100

    Add snowflake effect

---
+++ src/app/snowflakes/snowflakes.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-snowflakes',
+  imports: [],
+  templateUrl: './snowflakes.component.html',
+  styleUrl: './snowflakes.component.scss',
+})
+export class SnowflakesComponent {}
