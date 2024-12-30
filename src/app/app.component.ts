eac73217d5012a26220486a85ba9daeb461a3d9e
Velimir Đurković <velimir.djurkovic@gmail.com>
2024-12-30 15:39:16 +0100

    Move holiday service to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HolidayService } from './holiday.service';
+import { SnowflakesComponent } from './snowflakes/snowflakes.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    SnowflakesComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  private holidayService: HolidayService = inject(HolidayService);
+
+  isNewYearHolidaySeason = this.holidayService.isNewYearHolidaySeason();
   title = 'djvelimir.github.io';
 }
