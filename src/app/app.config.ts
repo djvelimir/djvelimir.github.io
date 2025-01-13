b6c278414d26c7c4c9cae9dc001ef9c644b99fc9
Velimir Đurković <velimir.djurkovic@gmail.com>
2025-01-13 02:50:46 +0100

    Add Angular route transition

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
-
 import { routes } from '@app/app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withHashLocation()),
     provideHttpClient(withFetch()),
+    provideAnimations(),
   ],
 };
--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { SnowflakesComponent } from '@components/snowflakes/snowflakes.component';
 import { HeaderComponent } from '@components/header/header.component';
 import { FooterComponent } from '@components/footer/footer.component';
 import { HolidayService } from '@services/holiday/holiday.service';
+import { routeTransition } from '@app/route-transition';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,15 @@ import { HolidayService } from '@services/holiday/holiday.service';
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  animations: [routeTransition],
 })
 export class AppComponent {
   isNewYearHolidaySeason = false;
 
-  constructor(private holidayService: HolidayService) {
+  constructor(
+    private holidayService: HolidayService,
+    protected route: ActivatedRoute
+  ) {
     this.holidayService.isNewYearHolidaySeason().subscribe({
       next: (value) => (this.isNewYearHolidaySeason = value),
       error: (err) => console.error(err),
