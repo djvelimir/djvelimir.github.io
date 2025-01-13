5bcf28641ecfffadfa785af6a8895f8949554074
Velimir Đurković <velimir.djurkovic@gmail.com>
2025-01-13 23:27:44 +0100

    Add contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { PortfolioComponent } from '@pages/portfolio/portfolio.component';
 import { AboutComponent } from '@pages/about/about.component';
+import { ContactComponent } from '@pages/contact/contact.component';
 import { PageNotFoundComponent } from '@pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: 'portfolio', title: 'Portfolio', component: PortfolioComponent },
   { path: 'about', title: 'About', component: AboutComponent },
+  { path: 'contact', title: 'Contact', component: ContactComponent },
   { path: '', redirectTo: '/portfolio', pathMatch: 'full' },
   { path: '**', title: 'Page Not Found', component: PageNotFoundComponent },
 ];
