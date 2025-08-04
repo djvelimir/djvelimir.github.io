7151873b57a87a11396649c62da1193aaf3639f7
Velimir Đurković <velimir.djurkovic@gmail.com>
2025-08-04 22:26:23 +0200

    feat: added installation instructions

--- src/app/pages/download/download.component.ts
+++ src/app/pages/download/download.component.ts
@@ -12,19 +12,19 @@ export class DownloadComponent {
     {
       title: 'Minesweeper',
       description:
-        'Download the Java Swing Minesweeper Game Demo! Ready to play the classic Minesweeper game with a sleek Java Swing interface? This demo brings Minesweeper to your desktop with: smooth tile revealing and mine-flagging gameplay, clean and intuitive user interface, satisfying sound effects to enhance the experience, exciting moments of victory that keep you coming back. No programming needed, just download, launch, and enjoy! Perfect for Minesweeper fans and casual gamers alike. Get the game now and start uncovering those mines!',
+        "Bring the classic Minesweeper experience to your desktop, now available with easy-to-use installers for all major platforms. Enjoy smooth tile revealing, precise mine flagging, and advanced features like chording for quick gameplay. The interface is clean and intuitive, enhanced with satisfying sound effects, a ticking timer, and optional background sounds. Numbered tiles are color-coded for clear visibility, just like in the original game. Choose your installer, run it, and start playing. Whether you're on Windows, macOS, or Linux, you're just a few clicks away from addictive puzzle-solving fun.",
       platforms: [
         {
           os: 'Linux',
-          title: 'Debian / RPM',
+          title: 'DEB / RPM',
           links: [
             {
-              description: 'For Ubuntu, Debian:',
+              description: 'For Debian, Ubuntu, Linux Mint:',
               title: 'minesweeper_1.0.0_amd64.deb',
               url: 'https://drive.google.com/file/d/1Y1cYiggd05p5rx86L4YbZcGkR9zj8JxE/view?usp=share_link',
             },
             {
-              description: 'For Fedora, CentOS:',
+              description: 'For Fedora, RHEL, CentOS:',
               title: 'minesweeper-1.0.0-1.x86_64.rpm',
               url: 'https://drive.google.com/file/d/1vyeGsuK2NlJPf4ArCFgG7k_ABdDvpk5S/view?usp=share_link',
             },
@@ -85,6 +85,26 @@ export class DownloadComponent {
           url: 'https://www.youtube.com/embed/UHiwIksxJ2A?rel=0',
         },
       ],
+      instructions: {
+        title: 'Installation Instructions',
+        platforms: [
+          {
+            title: 'Linux',
+            description:
+              "For Linux users, choose the package that matches your distribution. If you're using a Debian-based system like Debian, Ubuntu or Linux Mint, download the .deb file. You can install it using your system's software installer or a terminal with administrative privileges. On RPM-based systems like Fedora, RHEL or CentOS, download the .rpm file and use your system's package manager to install it. After installation, you can launch the game from your applications menu. If the game’s interface appears too small or improperly scaled on high-resolution screens, you can adjust the UI scaling by setting an environment variable. You can add the line export _JAVA_OPTIONS='-Dsun.java2d.uiScale=2.0' to your user’s shell configuration file (like ~/.bashrc or ~/.profile) for a per-user effect, or add it to a global configuration file (such as /etc/environment) to apply system-wide. After making this change, you will need to log out and log back in for the new environment variable to take effect.",
+          },
+          {
+            title: 'macOS',
+            description:
+              'On macOS, you have the option of using a .dmg or a .pkg installer. If you download the .dmg, open it and drag the Minesweeper app into your Applications folder. If you prefer the .pkg, simply double-click the file to run the guided installer. Once installed, the game will be available in Launchpad or your Applications folder.',
+          },
+          {
+            title: 'Windows',
+            description:
+              'To install the game on Windows, download either the .exe or .msi installer. Double-click the file to start the installation process and follow the setup wizard. Once installation is complete, you can launch the game from the Start Menu or your desktop.',
+          },
+        ],
+      },
     },
   ];
 }
